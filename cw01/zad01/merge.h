bf5ce1dfddbcb2b8dec05012c6774e0a768eda6a
Przemysław Kolaszyński <38491108+Szomeszp@users.noreply.github.com>
2021-03-17 13:01:32 +0100

    FINAL

--- cw01/zad01/merge.h
+++ cw01/zad01/merge.h
@@ -5,20 +5,6 @@
 #include <stddef.h>
 #include <string.h>
 
-//TODO:
-//Utworzenie tablicy wskaźników (tablicy głównej) — w tej tablicy będą przechowywane wskaźniki na wiersze zmergowanych plików — pierwszy element tablicy głównej zawiera wykaz wierszy pierwszej pary zmergowanych plików, drugi element dla drugiej pary, itd. Pojedynczy blok wierszy (element wskazywany z tablicy głównej), to tablica wskaźników na poszczególne wiersze w zmergowanym pliku
-//DONE - mainArray + createMAinTable + memoryBlock
-//Definiowanie sekwencji par plików
-//??
-//Przeprowadzenie zmergowania (dla każdego elementu sekwencji) oraz zapisanie wyniku zmergowania do pliku tymczasowego
-//
-//Utworzenie, na podstawie zawartość pliku tymczasowego, bloku wierszy — tablicy wskaźników na wiersze, ustawienie w tablicy głównej (wskaźników) wskazania na ten blok; na końcu, funkcja powinna zwrócić indeks elementu tablicy (głównej), który zawiera wskazanie na utworzony blok
-//Zwrócenie informacji o ilości wierszy w danym bloku wierszy
-//Usunięcie, z pamięci, bloku (wierszy) o zadanym indeksie
-//Usunięcie, z pamięci, określonego wiersza dla podanego bloku wierszy
-//Wypisanie zmergowanych plików, z uwzględnieniem wcześniejszych usuniętych bloków wierszy / wierszy
-
-
 typedef struct rowsBlock {
     char **rows;
     int size;
@@ -30,12 +16,12 @@ typedef struct mainArray {
 } mainArray_t;
 
 
-mainArray_t *createMainArray(int size); //Done?
+mainArray_t *createMainArray(int size);
 
-rowsBlock_t *createRowsBlock(FILE *mergedFile); //Done?
-FILE *mergeFiles(char *firstFile, char *secondFile); //Done?
+rowsBlock_t *createRowsBlock(FILE *mergedFile);
+FILE *mergeFiles(char *firstFile, char *secondFile);
 
-int numberOfRows(mainArray_t* mainArray, int blockIndex); //Done?
+int numberOfRows(mainArray_t* mainArray, int blockIndex);
 
 void removeBlock(mainArray_t* mainArray, int blockindex);
 void removeRow(mainArray_t* mainArray, int blockIndex, int row);
